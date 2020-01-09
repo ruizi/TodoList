1d4737c329dc0e1f577bb381f157f69b0adb0b53
YuKangYin <yukangyin@outlook.com>
2020-01-09 09:35:19 +0800

    delete comments in HomeView

--- TodoList/View/HomeView.swift
+++ TodoList/View/HomeView.swift
@@ -27,51 +27,6 @@ struct HomeView: View {// 默认展示登陆界面
                 LoginView()
                         .animation(.easeInOut)
             } else if (self.users.count == 1 && self.users[0].auth == "authorized") {
-
-//                var parameters: Dictionary = ["email": self.users[0].email,]
-//                Alamofire.request("https://ruitsai.tech/records_init_sync_api/", method: .post, parameters: parameters)
-//                        .responseJSON { response in
-//                            switch response.result.isSuccess {
-//                            case true:
-//                                if let value = response.result.value {
-//                                    let json = JSON(value)
-//
-//                                    print(json)
-//                                    // 把用户的日程信息存入数据库
-//                                    for (index, subJson): (String, JSON) in json {
-//                                        let detail = subJson["detail"].stringValue
-//                                        var Year = subJson["Year"].stringValue
-//                                        var Month = subJson["Month"].stringValue
-//                                        var Day = subJson["Day"].stringValue
-//
-//                                        let timestamp = subJson["timestamp"].stringValue
-//                                        let newTodoItem = TodoItem(context: self.managedObjectContext)
-//                                        newTodoItem.detail = detail
-//                                        newTodoItem.dueDate = Date(year: Int(Year) ?? 2020, month: Int(Month) ?? 01, day: Int(Day) ?? 10)
-//                                        newTodoItem.checked = false
-//                                        newTodoItem.timeStamp = timestamp  // 20200108234117
-//                                        // 使用CoreData保存
-//                                        do {
-//                                            try self.managedObjectContext.save()
-//                                        } catch {
-//                                            print("***")
-//                                            print(newTodoItem.detail)
-//                                            print(newTodoItem.dueDate)
-//                                            print(newTodoItem.checked)
-//                                            print(error)
-//                                        }
-//                                    }
-//
-//
-//                                } else {
-//                                    // TODO: 跳出一个弹框：network errors
-//                                    print("error happened")
-//                                }
-//                            case false:
-//                                print(response.result.error)
-//                            }
-//                        }
-
                 TodoListHomeView()
                         .animation(.easeInOut)
             }
